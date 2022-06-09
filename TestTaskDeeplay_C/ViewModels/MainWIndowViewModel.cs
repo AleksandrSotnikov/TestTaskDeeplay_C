c4d11bc38b2aec664303ccdf8828ede22c773a79
AleksandrSotnikov <sotnikovalex143@gmail.com>
2022-06-09 17:35:18 +0600

    Модель главного окна

---
+++ TestTaskDeeplay_C/ViewModels/MainWIndowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskDeeplay_C.ViewModels
+{
+    internal class MainWIndowViewModel : ViewModel
+    {
+        #region Title: String - Заголовок
+
+        private String _Title = "Главное окно программы";
+        public String Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+        
+    }
+}
